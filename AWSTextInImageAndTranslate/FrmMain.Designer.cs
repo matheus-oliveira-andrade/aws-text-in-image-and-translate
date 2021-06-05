641683c7feba39c2a1fc067dbac0db83d39b5a00
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-06-05 20:00:12 -0300

    Init

---
+++ AWSTextInImageAndTranslate/FrmMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace AWSTextInImageAndTranslate
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 362);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(547, 371);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.richTextBox2);
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Location = new System.Drawing.Point(1, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(547, 733);
+            this.panel2.TabIndex = 5;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(547, 356);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(554, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(725, 658);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.btnSelecionar);
+            this.panel1.Location = new System.Drawing.Point(554, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 69);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.Location = new System.Drawing.Point(488, 12);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(225, 47);
+            this.btnSelecionar.TabIndex = 6;
+            this.btnSelecionar.Text = "Selecionar imagem";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Click += new System.EventHandler(this.BtnSelecionar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Languages";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 734);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ AWSTextInImageAndTranslate/FrmMain.cs
@@ -0,0 +1,212 @@
+﻿using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AWSTextInImageAndTranslate
+{
+    public partial class FrmMain : Form
+    {
+        private AWSCredentials _awsCredentials;
+        private readonly RegionEndpoint _region = RegionEndpoint.USEast1;
+        private const string BUCKET_NAME = "aws-text-in-image-translate";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            comboBox1.DataSource = new BindingSource(GetLanguages(), null);
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
+
+        }
+
+        private async void BtnSelecionar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    pictureBox1.Load(openFileDialog1.FileName);
+
+                    await UploadImageToS3(openFileDialog1.FileName);
+                    List<string> detections = await DetectTextInImage(openFileDialog1.FileName);
+
+                    richTextBox1.Clear();
+                    richTextBox2.Clear();
+
+                    foreach (string detection in detections)
+                    {
+                        if (!string.IsNullOrEmpty(detection))
+                        {
+                            string selectedLanguage = comboBox1.SelectedValue.ToString() ?? "en";
+
+                            string translatedText = await TranslateText(detection, selectedLanguage);
+
+                            // text not translated
+                            richTextBox2.AppendText(detection + Environment.NewLine);
+
+                            // text transalated
+                            richTextBox1.AppendText(translatedText + Environment.NewLine);
+                        }
+                    }
+
+                    if (detections.Count <= 0)
+                        MessageBox.Show("Not recognized text in image");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async Task<string> TranslateText(string text, string language)
+        {
+            var client = new AmazonTranslateClient(_awsCredentials, _region);
+            TranslateTextRequest request = new TranslateTextRequest()
+            {
+                Text = text,
+                SourceLanguageCode = "auto",
+                TargetLanguageCode = language,
+            };
+
+            TranslateTextResponse result = await client.TranslateTextAsync(request);
+
+            return result.TranslatedText;
+        }
+
+        private async Task<List<string>> DetectTextInImage(string fileName)
+        {
+            var rekognitionClient = new AmazonRekognitionClient(_awsCredentials, _region);
+            var detectTextRequest = new DetectTextRequest()
+            {
+                Image = new Amazon.Rekognition.Model.Image()
+                {
+                    S3Object = new S3Object()
+                    {
+                        Name = Path.GetFileName(fileName),
+                        Bucket = BUCKET_NAME
+                    }
+                }
+            };
+
+            DetectTextResponse detectTextResponse = await rekognitionClient.DetectTextAsync(detectTextRequest);
+
+            List<string> detections = detectTextResponse.TextDetections.Where(td => td.Confidence > 80 && td.Type == TextTypes.LINE).Select(td => td.DetectedText).ToList();
+
+            foreach (TextDetection textDetection in detectTextResponse.TextDetections.Where(td => td.Type == TextTypes.WORD))
+            {
+                Pen pen = new Pen(Color.Yellow);
+
+                Graphics graphics = pictureBox1.CreateGraphics();
+
+                var boundingBox = textDetection.Geometry.BoundingBox;
+                var imageDimensions = pictureBox1.Image;
+
+                graphics.DrawRectangle(pen,
+                                       boundingBox.Left * imageDimensions.Width,
+                                       boundingBox.Top * imageDimensions.Height,
+                                       boundingBox.Width * imageDimensions.Width,
+                                       boundingBox.Height * imageDimensions.Height);
+            }
+
+            return detections;
+        }
+
+        private async Task<bool> UploadImageToS3(string file)
+        {
+            GetCredentials();
+
+            try
+            {
+                AmazonS3Client s3Client = new AmazonS3Client(_awsCredentials, _region);
+                TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+
+                await fileTransferUtility.UploadAsync(file, BUCKET_NAME);
+
+                return true;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                MessageBox.Show($"Error encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+        }
+
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+
+            if (!chain.TryGetAWSCredentials("AWS Educate profile", out _awsCredentials))
+            {
+                MessageBox.Show("Error fetching credentials");
+            }
+        }
+
+        private Dictionary<string, string> GetLanguages()
+        {
+            return new Dictionary<string, string>
+            {
+                { "Arabic", "ar" },
+                { "Armenian", "hy" },
+                { "Azerbaijani", "az" },
+                { "Bengali", "bn" },
+                { "Bosnian", "bs" },
+                { "Bulgarian", "bg" },
+                { "Catalan", "ca" },
+                { "Chinese (Simplified)", "zh" },
+                { "Chinese (Traditional)", "zh-TW" },
+                { "Croatian", "hr" },
+                { "Czech", "cs" },
+                { "Danish", "da" },
+                { "Dari", "fa-AF" },
+                { "Dutch", "nl" },
+                { "English", "en" },
+                { "Finnish", "fi" },
+                { "French", "fr" },
+                { "French (Canada)", "fr-CA" },
+                { "Hindi", "hi" },
+                { "Hungarian", "hu" },
+                { "Icelandic", "is" },
+                { "Indonesian", "id" },
+                { "Italian ", "it" },
+                { "Japanese", "ja" },
+                { "Korean", "ko" },
+                { "Portuguese", "pt" },
+                { "Slovak", "sk" },
+                { "Somali", "so" },
+                { "Spanish", "es" },
+                { "Spanish (Mexico)", "es-MX"},
+            };
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
